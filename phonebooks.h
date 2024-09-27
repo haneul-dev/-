d12cce01963b99bd71fb4a9381c372da0f8700d2
Kanghaneul <156298807+haneul-dev@users.noreply.github.com>
2024-09-27 23:47:53 +0900

    Create phonebooks.h

---
+++ phonebooks.h
@@ -0,0 +1,16 @@
+#ifndef PHONEBOOKS_H
+#define PHONEBOOKS_H
+
+// 연락처 구조체 정의
+typedef struct {
+    char name[50];  // 이름 필드
+    char phone[15]; // 전화번호 필드
+} Contact;
+
+// 함수 선언
+void insertContact(Contact *contacts, int *size);
+void deleteContact(Contact *contacts, int *size);
+void searchContact(Contact *contacts, int size);
+void listContacts(Contact *contacts, int size);
+
+#endif
